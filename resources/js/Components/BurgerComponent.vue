6a405d6cfe07d5cefaf2a5062d9e2aad15468bbd
Gytguz <gytguz@gmail.com>
2023-12-11 20:51:29 +0200

    Third commit

---
+++ resources/js/Components/BurgerComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>{{ routes }}</div>
+    <Link :href="route(routes)" class="hover:text-yellow-500 transition">
+        {{ names }}
+    </Link>
+
+
+
+
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { Link } from '@inertiajs/inertia-vue3';
+
+export default defineComponent({
+    props: [
+        'Link',
+        'routes',
+        'names',
+    ],
+});
+</script>
