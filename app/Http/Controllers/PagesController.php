db6a1f2fa28dc148be2b60ae25a611e53abe9748
Gytguz <gytguz@gmail.com>
2023-12-26 18:23:50 +0200

    Added filter by query string to shop

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -19,9 +19,21 @@ class PagesController extends Controller
         ]);
     }
 
-    public function shopPage() {
-        $products = Products::inRandomOrder()->get();
+    public function shopPage(Request $request)
+    {
         $categories = Categories::all();
+        $selectedCategory = $request->query('category');
+
+        // If a category is selected in the query string, filter products by that category
+        if ($selectedCategory) {
+            $products = Products::whereHas('categories', function ($query) use ($selectedCategory) {
+                $query->where('slug', $selectedCategory);
+            })->inRandomOrder()->get();
+        } else {
+            // If no category is selected, get all products
+            $products = Products::inRandomOrder()->get();
+        }
+
         return Inertia::render('Shop/Index', [
             'products' => $products,
             'categories' => $categories,
--- resources/js/Components/HeroComponent.vue
+++ resources/js/Components/HeroComponent.vue
@@ -1,29 +1,42 @@
 <template>
     <div class="relative">
-        <img src="/storage/images/hero_image.jpg" alt="Image cannot be found" class="relative object-cover h-96 w-full opacity-80">
-        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div class="flex flex-col items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row">
-                <ButtonComponent :href="category.slug" as="href" class="w-full py-4 sm:py-4" v-for="category in categories" :key="category.id">
-                    {{ category.name }}
-                </ButtonComponent>
-            </div>
+      <img src="/storage/images/hero_image.jpg" alt="Image cannot be found" class="relative object-cover h-96 w-full opacity-80">
+      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div class="flex flex-col items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row">
+          <ButtonComponent :href="changeQueryString(category)" as="href" class="w-full py-4 sm:py-4" v-for="category in categories" :key="category.id">
+            {{ category.name }}
+          </ButtonComponent>
         </div>
+      </div>
     </div>
-</template>
-
-<script>
-import { defineComponent } from 'vue';
-import ButtonComponent from './Buttons/ButtonComponent.vue';
-
-export default defineComponent({
-
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  import ButtonComponent from './Buttons/ButtonComponent.vue';
+  
+  export default defineComponent({
+  
     props: {
-        categories: Array,
+      categories: Array,
     },
-
+  
     components: {
-        ButtonComponent,
+      ButtonComponent,
     },
-
-});
-</script>
+  
+    data() {
+      return {
+        queryString: "",
+      };
+    },
+  
+    methods: {
+      changeQueryString(category) {
+        this.queryString = `/shop?category=${category.slug}`;
+        return this.queryString;
+      },
+    },
+  });
+  </script>
+  
