66c9a1e6a6658fb1e10bbbf30f1e31ecdce92948
Gytguz <gytguz@gmail.com>
2023-12-29 11:36:37 +0200

    Commit

--- resources/js/Components/ProductCardComponent.vue
+++ resources/js/Components/ProductCardComponent.vue
@@ -2,13 +2,13 @@
     <div class="bg-white">        
             <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    <a v-for="product in products" href="#" class="group">
+                    <Link v-for="product in products" :href="`/shop/product/${product.id}`" class="group">
                         <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                             <img :src="product.image" :alt="product.name" class="h-full w-full object-cover object-center group-hover:opacity-75">
                         </div>
                         <h3 class="mt-4 text-sm text-gray-700">{{ product.name }}</h3>
                         <p class="mt-1 text-lg font-medium text-gray-900">{{ product.price }}€</p>
-                    </a>
+                    </Link>
                 </div>
             </div>
     </div>
@@ -20,10 +20,15 @@
   
 <script>
 import { defineComponent } from 'vue';
+import { Link } from '@inertiajs/inertia-vue3';
 
 export default defineComponent({
-    props: [
-        'products',
-    ]
+    props: {
+        products: Array,
+    },
+
+    components: {
+            Link
+    },
 });
 </script>
